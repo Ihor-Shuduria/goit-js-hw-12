c2dfe36154b3c2f61b37483eb8f4b61fd2c528be
igor <igorshudurya@gmail.gom>
2025-08-17 09:16:38 +0200

    Почав виконувати домашне завдання

---
+++ src/main.js
@@ -0,0 +1,46 @@
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const query = e.target.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search term!',
+    });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (error) {
+    iziToast.error({
+      message: 'Something went wrong. Please try again later.',
+    });
+  } finally {
+    hideLoader();
+  }
+});
